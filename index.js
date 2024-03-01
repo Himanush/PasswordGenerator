16a10bde01a838b7f906b4a1b1f503ca47f2e64a
Himanush <himanushchintu@gmail.com>
2024-03-01 13:02:19 +0530

    modified JS

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const allChars = upperCase + lowerCase + numbers + symbols;
 function createPassword(){
 
     let password = "";
+
+    // It generates a random number from uppercase length i.e 0-25 and adds the value from each section uppercase,lowercase,numbers and sections
+    //This code only gives 4 characters so to get the length of 12 we use while loop combining all characters
     // password += upperCase[Math.floor(Math.random()*upperCase.length)];
     // password += lowerCase[Math.floor(Math.random()*lowerCase.length)];
     // password += numbers[Math.floor(Math.random()*numbers.length)];
@@ -24,6 +27,11 @@ function createPassword(){
 
 function copyPassword(){
     passwordBox.select();
-    //navigator.clipboard.writeText(passwordBox.value);
-    document.execCommand('copy');
+    //This method allows you to write text to the systems clipboard i.e temporary storage for data that user wants to copy from one place to another.
+    
+    navigator.clipboard.writeText(passwordBox.value);
+
+    //This method copies the selected text to clipboard
+    //it's deprecated i.e functionality is not recommended likely to br removed.
+    //document.execCommand('copy');
 }
